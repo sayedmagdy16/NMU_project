8d398e6814be1687fcd108b2a11aec471d22828c
sayed magdy abdallah ahmed <sayed.magdy.stu@nmu.edu.eg>
2022-05-19 21:56:28 +0200

    removed something

--- NMU project 1/Form2.Designer.cs
+++ NMU project 1/Form2.Designer.cs
@@ -72,7 +72,6 @@
             this.label2.Size = new System.Drawing.Size(83, 29);
             this.label2.TabIndex = 1;
             this.label2.Text = "Email";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
